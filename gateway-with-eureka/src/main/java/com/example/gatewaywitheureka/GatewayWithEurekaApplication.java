ad008b25e169044c3050dc1cbbf221023e4b1469
BambooKim <rlaqjarn1008@gmail.com>
2023-03-13 00:01:32 +0900

    Spring Cloud Gateway using Eureka for Service Discovery

---
+++ gateway-with-eureka/src/main/java/com/example/gatewaywitheureka/GatewayWithEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewaywitheureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayWithEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayWithEurekaApplication.class, args);
+    }
+
+}
---
+++ gateway-with-eureka/src/test/java/com/example/gatewaywitheureka/GatewayWithEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaywitheureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayWithEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
